c0b60fdf44a470a5866f9c6d5fa6fa2bad736008
Henrique de Souza La Porta <henriqueslaporta@gmail.com>
2017-11-18 17:01:36 -0200

    [Update] initFat() modificada

--- src/helpers/fat.c
+++ src/helpers/fat.c
@@ -14,30 +14,40 @@
 */
 
 void initFAT() {
-  CLUSTER_T reg;
-  unsigned int i, j;
-
-  /*
-  	Reserva os primeiros registros de Clusters por ser o super bloco e FAT
-  */
-  for (i = 0; i <= FAT_SECTOR; i++) {
-    config.indexFAT[i] = FAT_OCUPADO;
-  }
-
-  for (i = FAT_SECTOR + 1, i < constants.DISK_CLUSTERS; i++) {
+  SECTOR_T sec;
+  unsigned int i, j, index = 0;
+  unsigned char aux[4];
+  char* str;
 	
-	//Caso erro na leitura do cluster (i)
-    if(readCluster(i, &reg) != TRUE) {
-      config.indexFAT[i] = FAT_ERROR;
+	// Ler todos os setores do cluster 1
+  for (i = 0; i < constants.SECTOR_PER_CLUSTER; i++) {
+	
+	//Caso erro na leitura do de algum setor do cluster da fat
+    if(readSector(i, &sec) != TRUE) {
+    	printf("Erro durante a leitura de setores da FAT \n");
+		return;
     }
 	
-	/* 
-		Aqui apenas para continuar funcionando
-		Deve-se verificar se existe algo criado no disco
-	*/
-	config.indexFAT[i] = FAT_LIVRE;
-	
-  }
+	//"caminha" de 4 em 4 bytes pelo setor e salva o valor de acordo com o indice na FAT
+	for(j = 0; j <= FAT_SECTOR_SIZE-4; j += 4){
+		aux[0] = sec->at[j];
+		aux[1] = sec->at[j+1];
+		aux[2] = sec->at[j+2];
+		aux[3] = sec->at[j+3];
+		
+		int num =convertFourBytes(aux, 0, str);
+		
+		if(num != 1)		
+			config.indexFAT[index] = num;
+		else
+			config.indexFAT[index] = FAT_ERROR;
+			
+		index++;
+		//Garantia para não ocorrer Segmataton Fault no IndexFat
+		if(index >= constants.DISK_CLUSTERS)
+			return;
+	}//End FOR 2
+  }//End FOR 1
 }
 
 void printFAT(int begin, int end){
@@ -79,7 +89,7 @@ int setFAT(int clusterIndex, int allocated){
 
 int searchFAT(int allocated){
   unsigned int i;
-  for (i = REGISTER_REGULAR; i < constants.DISK_CLUSTERS; ++i)
+  for (i = 0; i < constants.DISK_CLUSTERS; ++i)
   {
     if (config.indexFAT[i] == allocated)
       return i;
